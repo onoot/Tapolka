b92711d15e991d1d844eda4bfb76e4d4e5886b80
unknown <penisxuei@gmail.com>
2024-11-07 14:24:43 +0700

    fix wallet button

--- src/components/UI/ButtonWallet/ButtonWallet.jsx
+++ src/components/UI/ButtonWallet/ButtonWallet.jsx
@@ -1,14 +1,22 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { TonConnectUI } from '@tonconnect/ui-react';
 import cl from './ButtonWallet.module.css';
 
 const ButtonWallet = () => {
     const [walletAddress, setWalletAddress] = useState(null);
-    const tonConnectUI = new TonConnectUI({ manifestUrl: 'https://app.tongaroo.fun/api/manifest/ton.json' });
+    const tonConnectUIRef = useRef(null); // Используем ref для хранения экземпляра TonConnectUI
+
+    useEffect(() => {
+        if (!tonConnectUIRef.current) {
+            tonConnectUIRef.current = new TonConnectUI({ 
+                manifestUrl: 'https://app.tongaroo.fun/api/manifest/ton.json' 
+            });
+        }
+    }, []);
 
     const connectWallet = async () => {
         try {
-            const wallet = await tonConnectUI.connectWallet(); // Ожидаем подключения
+            const wallet = await tonConnectUIRef.current.connectWallet();
             if (wallet?.account?.address) {
                 setWalletAddress(wallet.account.address);
             }
@@ -20,7 +28,6 @@ const ButtonWallet = () => {
     return (
         <div className={cl.test}>
             {walletAddress ? `Wallet: ${walletAddress}` : "Connect Wallet"}
-            {/* Кастомная кнопка */}
             <button className={cl.button_wallet} onClick={connectWallet}></button>
         </div>
     );
