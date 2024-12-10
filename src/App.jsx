8c9177fb7900934840b055c8f7193aa617d528f4
unknown <penisxuei@gmail.com>
2024-12-11 01:35:00 +0700

    add notifications

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import { useTelegram } from "./components/hooks/useTelegram";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 import Loading from "./pages/Loading";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 // Lazy imports
 const Navbar = React.lazy(() => import("./components/UI/Navbar/Navbar"));
@@ -24,6 +26,7 @@ function App() {
     // const urlBase="http://localhost"
 
     // const initData = {"query_id":"AAH-2XhEAAAAAP7ZeETH4IO6","user":{"id":1148770814,"first_name":"overlamer","last_name":"Broken","username":"Crazy_santa","language_code":"ru","allows_write_to_pm":true,"photo_url":"https://t.me/i/userpic/320/XO1rdazihmwfj8CcPcBSmaGmx1WgnQpPp5lJnxAxYQ0.svg"},"auth_date":"1731992070","signature":"1l_D07GmouWIa-mY0nG5EPDfISHvEl2UdYtsm41_M4jqeHlmGK6f2Oq6O6xjePbAsAk4yoo5i-ZRHvx3CF2-Bw","hash":"247557dc49adbf846e3a9ebd2dac18b59e0acde3d4371d8757608c89314aa3d9"}
+    
     useEffect(() => {
         tg.ready();
         expand();
@@ -44,14 +47,17 @@ function App() {
 
     // Функция для асинхронной загрузки данных игрока
     const fetchPlayerData = async () => {
+        toast.info('Загрузка данных игрока...');
         try {
             const response = await fetch(`${urlBase}/api/login`, {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(initData),
             });
-            if (!response.ok) 
+            if (!response.ok) {
+                toast.error('Ошибка при загрузке данных!');
                 return false;
+            }
 
             const data = await response.json();
             if (data.token) localStorage.setItem('token', data.token);
@@ -68,8 +74,9 @@ function App() {
                 benefit: data.benefit || 0,
             };
             updatePlayer(playerData);
+            toast.success('Данные игрока успешно загружены!');
         } catch (error) {
-            console.error("Error fetching player data:", error);
+            toast.error(`Ошибка: ${error.message}`);
         }
     };
 
@@ -80,6 +87,7 @@ function App() {
     return (
         <TonConnectUIProvider manifestUrl={`${urlBase}/manifest.json`}>
             <div className="App">
+            <ToastContainer />
                 {isLoading ? (
                     <Loading />
                 ) : (
